44ab0f2f898f57b9b1e1e24dfe0a508fadc0c4dc
Cassandra <cassandra+git@services.beelen.one>
2020-06-30 18:19:54 +0200

    Mess around with Travis CI x4

---
+++ utils/functions.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+mkdir -p cache
+
+should_build() {
+    # TODO: override:
+    return 0
+
+    file=cache/built.$(echo "$1" | sha256sum | cut -d " " -f 1)
+    if [ -e "$file" ]; then
+        return 1
+    fi
+    return 0
+}
+
+built() {
+    file=cache/built.$(echo "$1" | sha256sum | cut -d " " -f 1)
+    touch "$file"
+}
+
