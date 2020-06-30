300ce63d30f6fd1f91ff29943113b4d5e73d308a
Cassandra <cassandra+git@services.beelen.one>
2020-06-30 19:00:32 +0200

    Refactor CI config significantly

--- utils/push.sh
+++ utils/push.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# check we're not in a pull request
+if [ -n "$TRAVIS" ]; then
+  if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    return 0
+  fi
+fi
+
 # push to docker hub
 for image in $(docker images --format "{{.Repository}}:{{.Tag}}" | grep "${DOCKER_ROOT}"); do
   echo Pushing "${image}"...
