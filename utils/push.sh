6efacf7d5d1225ca0efdd8e237f73c58f78c2703
Cassandra <cassandra+git@services.beelen.one>
2020-09-01 11:30:16 +0200

    Fix push script

--- utils/push.sh
+++ utils/push.sh
@@ -14,8 +14,12 @@ fi
 # push to docker hub
 for image in $(docker images --format "{{.Repository}}:{{.Tag}}" | grep "${BUILD_ROOT}"); do
   echo Pushing "${image}" to Docker Hub...
-  docker push "${DOCKER_ROOT}/${image#"${BUILD_ROOT}"}" > /dev/null || exit 1
+  hub_image="${DOCKER_ROOT}/${image#"${BUILD_ROOT}"}"
+  docker tag "${image}" "${hub_image}"
+  docker push "${hub_image}" > /dev/null || exit 1
   echo Pushing "${image}" to Quay...
-  docker push "${QUAY_ROOT}/${image#"${BUILD_ROOT}"}" > /dev/null || exit 1
+  quay_image="${QUAY_ROOT}/${image#"${BUILD_ROOT}"}"
+  docker tag "${image}" "${quay_image}"
+  docker push "${quay_image}" > /dev/null || exit 1
   echo Pushed image.
 done
